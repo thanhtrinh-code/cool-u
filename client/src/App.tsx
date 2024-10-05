b7ff789e6697151c075afa7c853014784d17fca8
Elane Zhao <elane.zhao@highspot.com>
2024-10-05 13:07:00 -0700

    add a basic map with maplibre

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,16 @@
-import './App.css';
+import 'maplibre-gl/dist/maplibre-gl.css';
+import { RMap } from 'maplibre-react-components';
+
+const mountain: [number, number] = [6.4546, 46.1067];
 
 function App() {
   return (
-    <>
-      <h1>Cool U!</h1>
-    </>
+    <RMap
+      minZoom={6}
+      initialCenter={mountain}
+      initialZoom={8}
+      mapStyle="https://openmaptiles.geo.data.gouv.fr/styles/osm-bright/style.json"
+    />
   );
 }
 
